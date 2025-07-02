d29185656ae22ad008f102fbd4104c10bca77b68
Kayla Knelle <kaynell3234@gmail.com>
2025-07-02 11:04:17 -0400

    Just made a small change to the first console message

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is the change I have decided to make to the message.");
 Console.WriteLine("Why can't I be more creative with what to write?");
+Console.WriteLine("Also I just want to make sure I am uploading the URL right.");
