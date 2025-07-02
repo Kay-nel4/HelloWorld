9795354bd31b3341979cff87c8181d97dd1bdacb
Kayla Knelle <kaynell3234@gmail.com>
2025-07-02 10:48:37 -0400

    Created a new consolecalled HelloWorld and changed the message.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is the change I have decided to make to the message.");
+Console.WriteLine("Why can't I be more creative with what to write?");
